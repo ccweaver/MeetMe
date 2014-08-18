986c61b122338914943dc0669a9e9ff6395781a8
ccweaver <ccweaver@princeton.edu>
2014-08-18 18:14:02 -0400

    Initial commit from local git

---
+++ MeetMe/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  MeetMe
+//
+//  Created by Campbell Weaver on 8/18/14.
+//  Copyright (c) 2014 Campbell Weaver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MeetMe/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MeetMe
+//
+//  Created by Campbell Weaver on 8/18/14.
+//  Copyright (c) 2014 Campbell Weaver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XYZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XYZAppDelegate class]));
+    }
+}
